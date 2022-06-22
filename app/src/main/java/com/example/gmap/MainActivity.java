b05f20f2fd0eef95b94c8cfffc9ef17f68508d15
dokang0144 <yjjyk2004@gmail.com>
2022-06-22 19:24:49 +0900

    보물찾기 앱

--- app/src/main/java/com/example/gmap/MainActivity.java
+++ app/src/main/java/com/example/gmap/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.gmap;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -8,13 +11,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity
         implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
+    private LatLng SUNGIL = new LatLng(37.433774, 127.145662);
+    private double x, y;
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,21 +37,45 @@ public class MainActivity extends AppCompatActivity
 
         mMap = googleMap;
 
-        LatLng SEOUL = new LatLng(37.433774, 127.145662);
-
         MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(SEOUL);
+        markerOptions.position(SUNGIL);
         markerOptions.title("성일정보고등학교");
         markerOptions.snippet("성남시의 고등학교");
         mMap.addMarker(markerOptions);
 
-
-        // 기존에 사용하던 다음 2줄은 문제가 있습니다.
-        // CameraUpdateFactory.zoomTo가 오동작하네요.
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
-        //mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 10));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SUNGIL, 10));
 
     }
+    public LatLng getRandomLocation(LatLng c, int radius) {
+        double d2r = Math.PI / 180;
+        double r2d = 180 / Math.PI;
+        double earth_rad = 6378000f; //지구 반지름 근사값
 
+        double r = new Random().nextInt(radius) + new Random().nextDouble();
+        double rlat = (r / earth_rad) * r2d;
+        double rlng = rlat / Math.cos(c.latitude * d2r);
+
+        double theta = Math.PI * (new Random().nextInt(2) + new Random().nextDouble());
+        y = c.longitude + (rlng * Math.cos(theta));
+        x = c.latitude + (rlat * Math.sin(theta));
+        return new LatLng(x, y);
+    }
+
+    public void onClick(View view) {
+        getRandomLocation(SUNGIL, 1000);
+
+        count++;
+
+        LatLng RANDOM = new LatLng(x, y);
+
+        Toast.makeText(getApplicationContext(), "보물을 찾았습니다.", Toast.LENGTH_SHORT).show();
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(RANDOM);
+        markerOptions.title(count+"번째 보물");
+        markerOptions.snippet(count+"번째 보물입니다.");
+        mMap.addMarker(markerOptions);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(RANDOM, 17));
+    }
 }
